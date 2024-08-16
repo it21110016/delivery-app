c3c51b4bb295e22890d7eba3157dec3ba55f16b5
it21110016 <it21110016@my.sliit.lk>
2024-08-17 00:08:05 +0530

    implement basic form layout for state, city, and suburb information

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Form from '../components/Form';
+
+const Home = () => {
+  return (
+    <div className="min-h-screen bg-gray-100 p-4">
+      <Form />
+    </div>
+  );
+};
+
+export default Home;
