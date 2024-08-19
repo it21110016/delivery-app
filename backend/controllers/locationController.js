03e8d05b57cfe3e8aa9b5e5767f6c431f9b57435
it21110016 <it21110016@my.sliit.lk>
2024-08-19 22:30:48 +0530

    modify getCost method

--- backend/controllers/locationController.js
+++ backend/controllers/locationController.js
@@ -11,31 +11,38 @@ const createLocation = async (req, res) => {
     }
 };
 
-// Get all locations
-const getLocations = async (req, res) => {
-    try {
-        const locations = await Location.find();
-        res.status(200).json(locations);
-    } catch (err) {
-        res.status(400).json({ error: err.message });
-    }
-};
-
 // Get delivery cost by postal code
 const getCost = async (req, res) => {
     try {
-        const { postalCode } = req.params;
-        const locations = await Location.find({
-            'cities.suburbs.postalCodes.list': postalCode
+        const { postalCodes } = req.body;
+
+        if (!postalCodes) {
+            return res.status(400).json({ message: 'Postal code is required.' });
+        }
+
+        const location = await Location.findOne({
+            "cities.suburbs.postalCodes": postalCodes
         });
-        res.status(200).json(locations);
+
+        if (!location) {
+            return res.status(404).json({ message: "Postal code not found" });
+        }
+
+        const suburb = location.cities
+            .flatMap(city => city.suburbs)
+            .find(suburb => suburb.postalCodes === postalCodes);
+
+        if (!suburb) {
+            return res.status(404).json({ message: "Suburb not found" });
+        }
+
+        return res.status(200).json(suburb.delivery_costs);
     } catch (err) {
-        res.status(400).json({ error: err.message });
+        res.status(500).json({ error: err.message });
     }
 };
 
 module.exports = {
     createLocation,
-    getLocations,
     getCost
 };
