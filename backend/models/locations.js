13ced2615f9dfc04788f9a6b872907dafb3480b7
it21110016 <it21110016@my.sliit.lk>
2024-08-19 22:28:12 +0530

    add modified loaction model

--- backend/models/locations.js
+++ backend/models/locations.js
@@ -3,35 +3,38 @@ const { v4: uuidv4 } = require('uuid');
 
 const LocationSchema = new mongoose.Schema({
     stateId: { type: String, default: uuidv4 },
-    name: { type: String, required: true },
+    stateName: { type: String, required: true },
     cities: [
         {
             cityId: { type: String, default: uuidv4 },
-            name: { type: String, required: true },
+            cityName: { type: String, required: true },
             suburbs: [
                 {
                     suburbId: { type: String, default: uuidv4 },
-                    name: { type: String, required: true },
+                    suburbName: { type: String, required: true },
                     state: { type: String, required: true },
-                    postalCodes: {
-                        type: Map,
-                        of: String
-                    },
+                    postalCodeType: { type: String, required: true},
+                    postalCodes: { type: String, required: true},
                     delivery_costs: {
-                        type: Map,
-                        of: new mongoose.Schema({
-                            thresholds: {
-                                type: Map,
-                                of: Number
-                            },
-                            above_threshold: { type: Number, default: 0.00 }
-                        })
+                        thresholds:
+                            [
+                                {
+                                    orderValue: { type: Number },
+                                    cost: { type: Number },
+                                }
+
+                            ]
+                        ,
+                        above_threshold: { type: Number, default: 0.00 }
                     },
                     pickup_options: {
-                        thresholds: {
-                            type: Map,
-                            of: Number
-                        },
+                        thresholds: [
+                            {
+                                orderValue: { type: Number },
+                                cost: { type: Number },
+                            }
+
+                        ],
                         above_threshold: { type: Number, default: 0.00 }
                     }
                 }
